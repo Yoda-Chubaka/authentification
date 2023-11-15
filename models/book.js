b1f2f6ff6a709e9e012b0de058b799b8314518a7
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-11-15 16:21:42 +0200

    Update auth

--- models/book.js
+++ models/book.js
@@ -1,7 +1,7 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
-const {handleMongooseError} = require("../helpers");
+const { handleMongooseError } = require("../helpers");
 
 const genreList = ["fantastic", "love"];
 const dateRegexp = /^\d{2}-\d{2}-\d{4}$/;
@@ -29,8 +29,13 @@ const bookSchema = new Schema({
         // 16-10-2009
         match: dateRegexp,
         required: true,
-   }
-}, {versionKey: false, timestamps: true});
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    }
+}, { versionKey: false, timestamps: true });
 
 bookSchema.post("save", handleMongooseError);
 
